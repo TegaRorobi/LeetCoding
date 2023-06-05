66b1593fdaddf353aec69b0348d27b67b505261a
Tega Rorobi <rorobitega@gmail.com>
2023-06-05 23:40:07 +0100

    house-robberII, container-with-most-water

---
+++ Medium/container-with-most-water.py
@@ -0,0 +1,27 @@
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
+def maxArea(height):
+	# we initialize two pointers at the extreme ends of the array
+	l, r = 0, len(height)-1
+	curr_max = 0 
+
+	while l < r:
+		# now we compute the area with the distance between the pointers and the limiting value
+		area = (r-l) * min(height[l], height[r])
+		curr_max = max(curr_max, area)
+
+		# here we want to update the left pointer if it is the limiting pointer
+		if height[l] < height[r]:
+			l += 1
+		# here we also want to decrement the right pointer if it is the limiting pointer in 
+		# hope of finding a larger value. In an additional else case with this case, 
+		# where the values of both pointers are equal, we can move any of the pointers, 
+		# and that can be condensed into simply an else clause instead of elif and else
+		else:
+			r-=1
+	return curr_max
+
+
+print(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]))
---
+++ Medium/house-robberII.py
@@ -0,0 +1,22 @@
+
+# Time Complexity: O(2n-2) => O(n)
+# Space Complexity: constant i.e O(1)
+def robII(nums:list[int]) -> int:
+
+	def get_max_rob(houses):
+		rob1, rob2 = 0, 0 
+		for house in houses:
+			rob1, rob2 = rob2, max(house+rob1, rob2)
+		return rob2
+
+	# since the houses are now in a circle, we return the max loot from all the houses
+	# starting from the first house excluding the last house, or we could skip the first 
+	# house and then get the loot for all houses onwards. Afterwards, we return the max of 
+	# both loots avoiding a sticky situation where we rob adjacent houses and get busted!
+	# In the edge case where there is only one house, then we pass in empty arrays into the 
+	# get_max_rob helper function, and we get no loot, so adding the first value in the array
+	# which would then be the maximun takes care of this edge case quite neatly.
+	return max(nums[0], get_max_rob(nums[:-1]), get_max_rob(nums[1:]))
+
+
+print(robII([2, 3, 2]))
