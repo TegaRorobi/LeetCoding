8935637a172ffa67cb910f4004ccd848ecf4b941
Tega Rorobi <rorobitega@gmail.com>
2023-06-13 13:51:30 +0100

    Redo: Merge two sorted lists

--- Easy/merge-two-sorted-lists.py
+++ Easy/merge-two-sorted-lists.py
@@ -1,46 +1,45 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+    def __str__(self):
+        return f"ListNode({self.val}, {self.next})"
 
-class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        out_lst = []; out_nodes = None
-        
-        while list1:
-            out_lst.append(list1.val)
-            list1 = list1.next
-        while list2:
-            out_lst.append(list2.val)
-            list2 = list2.next 
-        
-        for i in sorted(out_lst)[::-1]:
-            out_nodes = ListNode(i, out_nodes)
-        return out_nodes
-        
 
-        ### Previous buggy, disgraceful and slow tries
 
-        # lst1,lst2 = [], []
-        # def unpack(listnode, to_list):
-            # to_list.append(listnode.val)
-            # if listnode.next == None: return 
-            # return unpack(listnode.next, to_list)
+def mergeTwoLists(list1: ListNode, list2: ListNode) -> ListNode:
+    # out_lst = []; out_nodes = None
+    # while list1:
+    #     out_lst.append(list1.val)
+    #     list1 = list1.next
+    # while list2:
+    #     out_lst.append(list2.val)
+    #     list2 = list2.next 
+    # for i in sorted(out_lst)[::-1]:
+    #     out_nodes = ListNode(i, out_nodes)
+    # return out_nodes
 
-        # unpack each input listnode into a variable
-        # unpack(list1, lst1); unpack(list2, lst2)
 
-        # join the listnodes together and sort
-        # ans_lst = sorted(lst1 + lst2)
+    dummy = curr = ListNode()
+    # print(f"Curr: {str(curr):45s}, Dummy: {dummy}\n\n") 
 
-        # def repack(val_lst):
-            # if len(val_lst) == 1:
-                # return ListNode(val_lst[0], None)
-            # return ListNode(val_lst[0], repack(val_lst[1:]))
-        
-        # pack the resulting list into a variable
-        # nodes = repack(ans_lst)
+    while list1 and list2:
+        if list1.val < list2.val:
+            curr.next = list1
+            curr, list1 = list1, list1.next
+            # print(f"Curr: {str(curr):45s}, Dummy: {dummy}\n\n") 
+        else:
+            curr.next = list2
+            curr, list2 = list2, list2.next
+            # print(f"Curr: {str(curr):45s}, Dummy: {dummy}\n\n") 
+    if list1 or list2:
+        curr.next = list1 if list1 else list2 
+    
+    return dummy.next 
 
-        # return the variable
-        # print(nodes)
+
+
+l1 = ListNode(1, ListNode(3, ListNode(4, None)))
+l2 = ListNode(1, ListNode(2, ListNode(4, None)))
+print(mergeTwoLists(l1, l2))
