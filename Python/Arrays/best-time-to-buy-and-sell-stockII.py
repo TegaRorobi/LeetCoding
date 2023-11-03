46f60e9dc407e8082bcdd6fec6ce7744cc1d99be
Tega Rorobi <rorobitega@gmail.com>
2023-11-03 09:45:21 +0100

    Fixed minor spacing and naming issues

--- Python/Arrays/best-time-to-buy-and-sell-stockII.py
+++ Python/Arrays/best-time-to-buy-and-sell-stockII.py
@@ -1,11 +1,11 @@
 # Medium
 
 def maxProfit(self, prices: List[int]) -> int:
-    maxprofit = 0
+    profit = 0
     last_stock = prices[0]
     for price in prices:
         if price > last_stock:
-            maxprofit += price-last_stock
+            profit += price-last_stock
         last_stock = price
-    return maxprofit
+    return profit
 
